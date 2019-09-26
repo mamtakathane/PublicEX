7c1c4c34eba9db2ec31a07a413b7576de315103d
mamta <mamta.kathane@gmail.com>
2019-09-26 16:39:34 +0530

    first commit

---
+++ src/com/jbk/Demo.java
@@ -0,0 +1,12 @@
+package com.jbk;
+
+public class Demo {
+	
+	public int a=10;
+	 int b;
+
+	 public void display(){
+		 
+		 System.out.println(a);
+	 }
+}
---
+++ src/com/jbk/Demo2.java
@@ -0,0 +1,11 @@
+package com.jbk;
+
+public class Demo2 extends Demo{
+	public static void main(String[] args) {
+	Demo2 d = new Demo2();
+		//System.out.println(d.a);
+		d.display();
+		
+	}
+
+}
